4a952b74c26c5cf6eba804c4ab50e01d2e436d1f
Mosquito <EvanChen99@outlook.com>
2022-10-11 00:45:19 -0400

    test read xlsx

--- highlight.py
+++ highlight.py
@@ -21,7 +21,7 @@ dataframe = openpyxl.load_workbook("index.xlsx")
 dataframe1 = dataframe.active
  
 # Iterate the loop to read the cell values
-for row in dataframe1.iter_cols(1, 4):
+for row in dataframe1.iter_rows(1, 4):
     for col in range(4, dataframe1.max_column):
         keyword = row[2].value
         print(type(keyword), keyword)
