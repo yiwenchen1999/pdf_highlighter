dfe33e3a03059a0e84459bd6f1d53bd2ae979dcb
Mosquito <EvanChen99@outlook.com>
2022-10-11 01:24:51 -0400

    add unfound record

--- highlight.py
+++ highlight.py
@@ -1,15 +1,27 @@
 import fitz
 import openpyxl
+import json
+
+unfound = {}
 
 def hightligh_in_page(doc, index, keyword):
+    found = False
     for i in range(50):
         page = doc[max(len(doc), index-25+i)]
 
         text_instances = page.search_for(keyword)
         for inst in text_instances:
+            found = True
             print("found istance on page:", index-25+i)
             highlight = page.add_highlight_annot(inst)
             highlight.update()
+    if not found:
+        if keyword in unfound:
+            unfound[keyword].append(index)
+        else:
+            num = []
+            num.append(index)
+            unfound[keyword] = num
 
 ### READ IN PDF
 doc = fitz.open("history-of-western-phil.pdf")
@@ -42,3 +54,6 @@ for row in dataframe1.iter_rows(1, dataframe1.max_row):
 
 ### OUTPUT
 doc.save("highlighted.pdf", garbage=4, deflate=True, clean=True)
+with open('unfound.txt', 'w') as convert_file:
+     convert_file.write(json.dumps(unfound))
+
