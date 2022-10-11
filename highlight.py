1df9cda3afd0bef92657a62a8d4e78955f1d89c1
Mosquito <EvanChen99@outlook.com>
2022-10-11 00:41:12 -0400

    test read xlsx

--- highlight.py
+++ highlight.py
@@ -21,9 +21,11 @@ dataframe = openpyxl.load_workbook("index.xlsx")
 dataframe1 = dataframe.active
  
 # Iterate the loop to read the cell values
-for row in range(0, 4):
-    for col in dataframe1.iter_cols(1, dataframe1.max_column):
-        print(col[row].value)
+for row in dataframe1.iter_cols(1, dataframe1.max_row):
+    for col in range(3, dataframe1.max_column):
+        keyword = row[col].value
+        print(type(keyword), keyword)
+        print(row[col].value)
 
 ### OUTPUT
 doc.save("output.pdf", garbage=4, deflate=True, clean=True)
