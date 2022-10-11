4f733c2cd9cb86369e7d9a98f8662590f71806bc
Mosquito <EvanChen99@outlook.com>
2022-10-11 00:32:48 -0400

    test read xlsx

--- highlight.py
+++ highlight.py
@@ -1,18 +1,29 @@
 import fitz
+import openpyxl
 
 ### READ IN PDF
-doc = fitz.open("random.pdf")
+doc = fitz.open("history-of-western-phil.pdf")
 
-for page in doc:
-    ### SEARCH
-    text = "information"
-    text_instances = page.searchFor(text)
+# for page in doc:
+#     ### SEARCH
+#     text = "information"
+#     text_instances = page.searchFor(text)
 
-    ### HIGHLIGHT
-    for inst in text_instances:
-        highlight = page.addHighlightAnnot(inst)
-        highlight.update()
-
+#     ### HIGHLIGHT
+#     for inst in text_instances:
+#         highlight = page.addHighlightAnnot(inst)
+#         highlight.update()
+ 
+# Define variable to load the dataframe
+dataframe = openpyxl.load_workbook("index.xlsx")
+ 
+# Define variable to read sheet
+dataframe1 = dataframe.active
+ 
+# Iterate the loop to read the cell values
+for row in range(0, 4):
+    for col in dataframe1.iter_cols(1, dataframe1.max_column):
+        print(col[row].value)
 
 ### OUTPUT
 doc.save("output.pdf", garbage=4, deflate=True, clean=True)
